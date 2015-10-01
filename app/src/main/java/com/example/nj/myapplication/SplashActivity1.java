7ef22ffa3296e82c23972a20b3141d24a9a4e64f
nakjun <njsung1217@gmail.com>
2015-10-01 19:21:30 +0900

    started
    real

---
+++ app/src/main/java/com/example/nj/myapplication/SplashActivity1.java
@@ -0,0 +1,49 @@
+package com.example.nj.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class SplashActivity1 extends AppCompatActivity {
+
+    Intent intent;
+    TextView result_text;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash1);
+
+        intent = getIntent();
+        int yes_count = intent.getIntExtra("YesCount",0);
+        int no_count = intent.getIntExtra("NoCount",0);
+
+        result_text = (TextView)findViewById(R.id.textView_result);
+        result_text.setText("YES를 "+yes_count+"개 선택하셨습니다.\n" +
+                "더 많은 YES를 선택하고 싶다면 다음 장으로 이동해주세요");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_splash_activity1, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
