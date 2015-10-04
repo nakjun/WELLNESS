95a895e5b19722ebd9782dc5bc23cc2a56b024b5
Ben <ben399399@naver.com>
2015-10-04 17:15:27 +0900

    민상
    modified base activity

--- app/src/main/java/com/example/nj/myapplication/DW_Activity/DW_Activity.java
+++ app/src/main/java/com/example/nj/myapplication/DW_Activity/DW_Activity.java
@@ -1,5 +1,6 @@
 package com.example.nj.myapplication.DW_Activity;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,23 +10,16 @@ import android.view.View;
 
 import com.example.nj.myapplication.R;
 
-public class DW_Activity extends AppCompatActivity {
+public class DW_Activity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dw_);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        /*TODO
+            create activity
+        */
     }
 
 }
