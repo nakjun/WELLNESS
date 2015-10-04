2886dc634ddae7165417c58b8d014b9ea6cb96f7
Ben <ben399399@naver.com>
2015-10-04 18:33:17 +0900

    민상
    dw_main_initialized

--- app/src/main/java/com/example/nj/myapplication/DW_Activity/DW_Activity.java
+++ app/src/main/java/com/example/nj/myapplication/DW_Activity/DW_Activity.java
@@ -2,25 +2,38 @@ package com.example.nj.myapplication.DW_Activity;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.nj.myapplication.R;
 
 public class DW_Activity extends Activity {
-
+    TextView title;
+    ImageView image1,image2,image3,image4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dw_);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        /*TODO
-            create activity
-        */
+        title=(TextView)findViewById(R.id.dw_main_title);
+        title.setText(R.string.dw_title);
+        title.setTextSize(32);
+        image1=(ImageView)findViewById(R.id.dw_main_image1);
+        image2=(ImageView)findViewById(R.id.dw_main_image2);
+        image3=(ImageView)findViewById(R.id.dw_main_image3);
+        image4=(ImageView)findViewById(R.id.dw_main_image4);
+        image1.setImageResource(R.drawable.dw_main1);
+        image2.setImageResource(R.drawable.dw_main2);
+        image3.setImageResource(R.drawable.dw_main3);
+        image4.setImageResource(R.drawable.dw_main4);
+
+
+        //TODO create activity
+
+
     }
+
+
 
 }
 
