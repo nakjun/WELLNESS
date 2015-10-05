16b7bb45de5f1fd7d5f40da24d387b4ba28249fa
Ben <ben399399@naver.com>
2015-10-05 16:49:43 +0900

    dw_main_modified

--- app/src/main/java/com/example/nj/myapplication/DW_Activity/DW_Activity.java
+++ app/src/main/java/com/example/nj/myapplication/DW_Activity/DW_Activity.java
@@ -2,7 +2,11 @@ package com.example.nj.myapplication.DW_Activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -14,6 +18,7 @@ public class DW_Activity extends Activity {
     TextView title;
     ImageView image1,image2,image3,image4;
     Button button;
+    int width;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -27,10 +32,14 @@ public class DW_Activity extends Activity {
         image2=(ImageView)findViewById(R.id.dw_main_image2);
         image3=(ImageView)findViewById(R.id.dw_main_image3);
         image4=(ImageView)findViewById(R.id.dw_main_image4);
-        image1.setImageResource(R.drawable.dw_main1);
-        image2.setImageResource(R.drawable.dw_main2);
-        image3.setImageResource(R.drawable.dw_main3);
-        image4.setImageResource(R.drawable.dw_main4);
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        width = size.x;
+        image1.setImageBitmap(Bitmap.createScaledBitmap(BitmapFactory.decodeResource(this.getResources(), R.drawable.dw_main1), width/2, width/2, false));
+        image2.setImageBitmap(Bitmap.createScaledBitmap(BitmapFactory.decodeResource(this.getResources(), R.drawable.dw_main2), width/2, width/2, false));
+        image3.setImageBitmap(Bitmap.createScaledBitmap(BitmapFactory.decodeResource(this.getResources(), R.drawable.dw_main3), width/2, width/2, false));
+        image4.setImageBitmap(Bitmap.createScaledBitmap(BitmapFactory.decodeResource(this.getResources(), R.drawable.dw_main4), width/2, width/2, false));
 
         button=(Button)findViewById(R.id.dw_main_button);
         button.setTextSize(32);
