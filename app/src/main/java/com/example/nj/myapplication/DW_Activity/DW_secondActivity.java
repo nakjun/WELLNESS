d59da059cb50c6aafd7637aff96896d8b15f9c81
Ben <ben399399@naver.com>
2015-10-05 15:36:35 +0900

    dw_sec_almost

--- app/src/main/java/com/example/nj/myapplication/DW_Activity/DW_secondActivity.java
+++ app/src/main/java/com/example/nj/myapplication/DW_Activity/DW_secondActivity.java
@@ -6,18 +6,27 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.example.nj.myapplication.R;
 
 public class DW_secondActivity extends AppCompatActivity {
-
+    TextView top,mid,bot;
+    ImageButton back,center;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dw_second);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        top=(TextView)findViewById(R.id.dw_sec_topText);
+        mid=(TextView)findViewById(R.id.dw_sec_midText);
+        bot=(TextView)findViewById(R.id.dw_sec_botText);
+        top.setText(R.string.dw_2nd_top);
+        mid.setText(R.string.dw_2nd_mid);
+        mid.setTextSize(32);
+        bot.setText(R.string.dw_2nd_bottom);
     }
+
+
 
 }
