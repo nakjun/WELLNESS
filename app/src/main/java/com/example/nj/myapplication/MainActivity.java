743b6c2ea74d4fa84ff82e3f1503218e21303ed6
nakjun <njsung1217@gmail.com>
2015-10-07 14:44:49 +0900

    MainActivity수정
    Thread를 통한 DB접속 코드 적용

--- app/src/main/java/com/example/nj/myapplication/MainActivity.java
+++ app/src/main/java/com/example/nj/myapplication/MainActivity.java
@@ -2,15 +2,26 @@ package com.example.nj.myapplication;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.nj.myapplication.DW_Activity.DW_Activity;
 import com.example.nj.myapplication.YN_Activity.YesNoActivity;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends Activity {
     Button btn_login,btn_join;
@@ -58,5 +69,58 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    private class phpDown extends AsyncTask<String, Integer,String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            StringBuilder jsonHtml = new StringBuilder();
+            try{
+                // 연결 url 설정
+                URL url = new URL(urls[0]);
+                // 커넥션 객체 생성
+                HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                // 연결되었으면.
+                if(conn != null){
+                    conn.setConnectTimeout(10000);
+                    conn.setUseCaches(false);
+                    // 연결되었음 코드가 리턴되면.
+                    if(conn.getResponseCode() == HttpURLConnection.HTTP_OK){
+                        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+                        for(;;){
+                            // 웹상에 보여지는 텍스트를 라인단위로 읽어 저장.
+                            String line = br.readLine();
+                            if(line == null) break;
+                            // 저장된 텍스트 라인을 jsonHtml에 붙여넣음
+                            jsonHtml.append(line + "\n");
+                        }
+                        br.close();
+                    }
+                    conn.disconnect();
+                }
+            } catch(Exception ex){
+                ex.printStackTrace();
+            }
+            return jsonHtml.toString();
+        }
+
+        /*
+        protected void onPostExecute(String str){
+            txtView.setText(str);
+        }
+        */
+
+        protected void onPostExecute(String str){
+            if(str.equals("true"))
+            {
+                Toast.makeText(getApplicationContext(),"Login Success.",Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+                Toast.makeText(getApplicationContext(),"Login Fail.",Toast.LENGTH_SHORT).show();
+            }
+
+        }
+
     }
 }
