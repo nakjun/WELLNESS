d1b011b79f1d0bdefaff1e3ce75f0ed71d932308
Ben <ben399399@naver.com>
2015-11-01 21:42:18 +0900

    temporaryasd

--- app/src/main/java/com/example/nj/myapplication/DW_Activity/DW_RealBreatheActivity.java
+++ app/src/main/java/com/example/nj/myapplication/DW_Activity/DW_RealBreatheActivity.java
@@ -3,6 +3,7 @@ package com.example.nj.myapplication.DW_Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -14,6 +15,7 @@ public class DW_RealBreatheActivity extends AppCompatActivity {
     ImageView cenImg;
     Thread changerThread;
     Bitmap one,two,three;
+    MediaPlayer player;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,18 +28,59 @@ public class DW_RealBreatheActivity extends AppCompatActivity {
         two=(Bitmap.createScaledBitmap(BitmapFactory.decodeResource(this.getResources(), R.drawable.dw_breathe_7sec), 900, 1000, false));
         three=(Bitmap.createScaledBitmap(BitmapFactory.decodeResource(this.getResources(), R.drawable.dw_breathe_8sec), 900, 1000, false));
 
+
         changerThread=new Thread(new Runnable() {
             @Override
-            public void run() {
+            synchronized public void run() {
+                player= MediaPlayer.create(getApplicationContext(), R.raw.dw_narration_breathe_intro);
+                player.start();
+
+                try {
+                    wait(11500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 for(int i=0;i<3;i++){
-                    cenImg.setImageBitmap(one);
+                    cenImg.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            cenImg.setImageBitmap(one);
+                        }
+                    });
+
+                    player= MediaPlayer.create(getApplicationContext(), R.raw.dw_narration_breathe_4sec);
+                    player.start();
                     try {
-                        wait();
+                        wait(4000);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-                    
-
+                    cenImg.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            cenImg.setImageBitmap(two);
+                        }
+                    });
+                    player= MediaPlayer.create(getApplicationContext(), R.raw.dw_narration_breathe_7sec);
+                    player.start();
+                    try {
+                        wait(7000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    cenImg.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            cenImg.setImageBitmap(three);
+                        }
+                    });
+                    player= MediaPlayer.create(getApplicationContext(), R.raw.dw_narration_breathe_8sec);
+                    player.start();
+                    try {
+                        wait(8000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
                 //startActivity(new Intent(DW_RealBreatheActivity.this, ***.class));
             }
