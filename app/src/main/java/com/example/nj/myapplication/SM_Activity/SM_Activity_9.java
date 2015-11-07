e7c9491ab3a6fc17829c35a9ac9b94304613aa0c
Ben <ben399399@naver.com>
2015-11-08 03:47:03 +0900

    파이널퓨전
    배고파요

---
+++ app/src/main/java/com/example/nj/myapplication/SM_Activity/SM_Activity_9.java
@@ -0,0 +1,21 @@
+package com.example.nj.myapplication.SM_Activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.example.nj.myapplication.R;
+
+public class SM_Activity_9 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sm_activity_9);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+}
